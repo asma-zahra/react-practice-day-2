03c526dc140239ea828d7dd18b08064fdd5ccd2c
Asma <asmazahra7860@gmail.com>
2023-01-03 18:39:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading1 = React.createElement(
+    "h1",
+    {id: "title",},
+    "heading1 from parcel"
+);
+const heading2 = React.createElement(
+    "h2",
+    {id: "title",},
+    "heading2 from parcel"
+);
+const container = React.createElement(
+    "div",
+    {id: "container",},
+    [heading1 ,heading2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
